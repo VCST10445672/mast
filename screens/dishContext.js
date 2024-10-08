7240008639389cf54b589111d19419fdf6b34098
st10445672 <160609588+VCST10445672@users.noreply.github.com>
2024-10-08 20:49:41 +0200

    Update dishContext.js

--- screens/dishContext.js
+++ screens/dishContext.js
@@ -1,21 +1,21 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';    //save
 
 const DishesContext = createContext();
 
-export const DishesProvider = ({ children }) => {
+export const DishesProvider = ({ children }) => {    //save
   const [dishes, setDishes] = useState([]);
 
   const addDish = (dish) => {
     setDishes((prevDishes) => [...prevDishes, dish]);
   };
 
-  return (
+  return (    //The IIE
     <DishesContext.Provider value={{ dishes, addDish }}>
       {children}
     </DishesContext.Provider>
   );
 };
 
-export const useDishes = () => {
+export const useDishes = () => {    //The IIE
   return useContext(DishesContext);
 };
